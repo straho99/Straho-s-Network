1d382ce8dbc00c4c878c6c86fdcafde6ee90b154
straho99 <strahil.ruychev@gmail.com>
2015-05-18 16:43:56 +0300

    Logout done

--- scripts/controllers/userHeaderController.js
+++ scripts/controllers/userHeaderController.js
@@ -1,4 +1,19 @@
 socialNetwork.controller('UserHeaderController',
-    function UserHeaderController($scope, authentication) {
+    function UserHeaderController($scope, $location, authentication, notify) {
         $scope.username = authentication.getUserName();
+
+        $scope.logout = function () {
+            authentication.logout()
+                .then(
+                function successHandler(data) {
+                    authentication.clearCredentials();
+                    notify.info("Logout successful.");
+                    $location.path('/');
+
+                },
+                function errorHandler(error) {
+                    notify.error("Logout failed.");
+                }
+            );
+        }
     });
