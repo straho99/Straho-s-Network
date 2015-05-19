722630668937b490392c7849de1e2f301fd1f7d4
straho99 <strahil.ruychev@gmail.com>
2015-05-19 18:30:38 +0300

    Service added - profileData

--- scripts/services/profileData.js
+++ scripts/services/profileData.js
@@ -1,18 +1,49 @@
 'use strict';
 
 socialNetwork.factory('profileData', function profileData($http, requester, authentication , baseServiceUrl) {
-    var serviceUrl = baseServiceUrl + 'posts';
+    var service = {},
+        serviceUrl = baseServiceUrl + 'me';
 
-    function getPostById(id) {
+    service.getDataAboutMe = function () {
+        return requester('GET', serviceUrl, this.getHeaders());
+    };
 
-        return requester('GET', serviceUrl + '/' + id, authentication.getHeaders());
-    }
+    service.getOwnFriends = function () {
+        return requester('GET', serviceUrl + '/friends', this.getHeaders());
+    };
 
+    service.getFriendRequests = function () {
+        return requester('GET', serviceUrl + '/requests', this.getHeaders());
+    };
 
+    service.sendFriendRequest = function (username) {
+        return requester('POST', serviceUrl + '/requests/' + username, this.getHeaders());
+    };
 
+    service.approveFriendRequest = function (requestId) {
+        return requester('PUT', serviceUrl + '/requests/' + requestId + '?status=approved', this.getHeaders());
+    };
 
+    service.rejectFriendRequest = function (requestId) {
+        return requester('PUT', serviceUrl + '/requests/' + requestId + '?status=delete', this.getHeaders());
+    };
+
+    service.getNewsFeedPages = function (StartPostId) {
+        return requester('GET', serviceUrl + '/feed?StartPostId=' + StartPostId + '&PageSize=5', this.getHeaders());
+    };
+
+    service.changePassword = function (passwordData) {
+        return requester('PUT', serviceUrl + '/changepassword', this.getHeaders(), passwordData);
+    };
 
     return {
-
+        getDataAboutMe: getDataAboutMe,
+        getOwnFriends: getOwnFriends,
+        getFriendsRequests: getFriendRequests,
+        sendFriendRequest: sendFriendRequest,
+        approveFriendRequest: approveFriendRequest,
+        rejectFriendRequest: rejectFriendRequest,
+        getNewsFeedPages: getNewsFeedPages,
+        changePassword: changePassword
     }
 });
